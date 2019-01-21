307c4ece08639b299d194477cbf08fd8f1ef7427
nlp.baoy <nlp.baoy@gmail.com>
2019-01-21 10:22:41 +0800

    clean the code

--- test/eval_test.py
+++
@@ -1,9 +0,0 @@
-import sys
-
-sys.path.append(".")
-from metrics.bleu_scorer import BleuScoreEvaluator
-
-pred_file = sys.argv[1]
-gold_file = sys.argv[2]
-
-print(BleuScoreEvaluator.evaluate_file(pred_file=pred_file, gold_files=gold_file))
--- test/view_test.py
+++
@@ -1,9 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-
-x1 = np.arange(0, 5, 0.1)
-y1 = np.sin(x1)
-x2 = np.linspace(1, 10, 20, True)
-y2 = np.cos(x2)
-
-plt.plot(x1, y1, 'b^')
